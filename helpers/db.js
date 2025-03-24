54b6dde6e0d6f48f84fcb2715aa987b32d3f132c
ashrafsakib <ashrafulsakib740@gmail.com>
2025-03-24 07:55:00 -0700

    Changes

--- helpers/db.js
+++ helpers/db.js
@@ -1,19 +1,16 @@
 const { Pool } = require('pg');
 
 // Creating Pool
-const openDb = () => {
-    const pool = new Pool({
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT,
-        ssl: {
-            rejectUnauthorized: false,
-        },
-    });
-    return pool;
-};
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
 
 const query = async (sql, values = []) => {
     try {
